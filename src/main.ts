95e4115d0e01809933ddc09341e4feb5e4b83878
TheOpti <wieckowski.piotr.93@gmail.com>
2025-07-23 22:51:41 +0200

    Move code from my previous repo

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,170 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+export const DIRECTIONS = {
+  RIGHT: 'right',
+  LEFT: 'left',
+  UP: 'up',
+  DOWN: 'down',
+};
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+export const DIMENSIONS = {
+  WIDTH: 800,
+  HEIGHT: 600,
+};
+
+export const FRAME_LENGTH = 150;
+
+const canvasElement = document.getElementById('canvas');
+
+canvasElement.setAttribute('width', DIMENSIONS.WIDTH);
+canvasElement.setAttribute('height', DIMENSIONS.HEIGHT);
+
+var canvasContext = canvasElement.getContext('2d');
+
+const snake = [];
+let food = {};
+let points = 0;
+
+let currentDirection = DIRECTIONS.RIGHT;
+
+function initializeGame() {
+  document.addEventListener('keydown', checkKey);
+
+  createSnake();
+  generateFood();
+  gameLoop();
+}
+
+function gameLoop() {
+  canvasContext.fillStyle = 'white';
+  canvasContext.fillRect(0, 0, DIMENSIONS.WIDTH, DIMENSIONS.HEIGHT);
+  canvasContext.strokeStyle = 'black';
+  canvasContext.strokeRect(0, 0, DIMENSIONS.WIDTH, DIMENSIONS.HEIGHT);
+
+  const next = getNextSnakePosition();
+
+  // Get last element from snake and set it as first position
+  const newPos = snake.pop();
+  newPos.x = next.X;
+  newPos.y = next.Y;
+  snake.unshift(newPos);
+  drawSnake();
+
+  paintCell(food.x, food.y);
+  checkCollision();
+  setTimeout(gameLoop, FRAME_LENGTH);
+}
+
+function createSnake() {
+  var length = 15;
+  for (var i = length; i > 10; i--) {
+    snake.push({ x: i, y: 5 });
+  }
+}
+
+function getNextSnakePosition() {
+  switch (currentDirection) {
+    case DIRECTIONS.RIGHT: {
+      return {
+        X: snake[0].x + 1,
+        Y: snake[0].y + 0,
+      };
+    }
+    case DIRECTIONS.LEFT: {
+      return {
+        X: snake[0].x - 1,
+        Y: snake[0].y + 0,
+      };
+    }
+    case DIRECTIONS.UP: {
+      return {
+        X: snake[0].x + 0,
+        Y: snake[0].y + 1,
+      };
+    }
+    case DIRECTIONS.DOWN: {
+      return {
+        X: snake[0].x + 0,
+        Y: snake[0].y - 1,
+      };
+    }
+  }
+}
+
+function checkKey(event) {
+  if (event.keyCode === 38 && currentDirection !== DIRECTIONS.UP) {
+    currentDirection = DIRECTIONS.DOWN;
+  } else if (event.keyCode === 40 && currentDirection !== DIRECTIONS.DOWN) {
+    currentDirection = DIRECTIONS.UP;
+  } else if (event.keyCode === 37 && currentDirection !== DIRECTIONS.RIGHT) {
+    currentDirection = DIRECTIONS.LEFT;
+  } else if (event.keyCode === 39 && currentDirection !== DIRECTIONS.LEFT) {
+    currentDirection = DIRECTIONS.RIGHT;
+  }
+}
+
+function drawSnake() {
+  snake.forEach(({ x, y }) => {
+    paintCell(x, y);
+  });
+}
+
+function generateFood() {
+  food = {
+    x: Math.round((Math.random() * (DIMENSIONS.WIDTH - 20)) / 20),
+    y: Math.round((Math.random() * (DIMENSIONS.HEIGHT - 20)) / 20),
+  };
+  if (!checkIfFoodXYCorrect()) {
+    generateFood();
+  }
+}
+
+function clearFood() {
+  canvasContext.fillStyle = 'white';
+  canvasContext.fillRect(food.x * 20, food.y * 20, 20, 20);
+  canvasContext.strokeStyle = 'white';
+  canvasContext.strokeRect(food.x * 20, food.y * 20, 20, 20);
+}
+
+function checkIfFoodXYCorrect() {
+  for (var i = 0; i < snake.length; i++) {
+    if (snake[i].x === food.x && snake[i].y === food.y) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function checkCollision() {
+  const { x: snakeX, y: snakeY } = snake[0];
+
+  const isOutOfBounds =
+    snakeX < 0 ||
+    snakeX > DIMENSIONS.WIDTH / 20 ||
+    snakeY < 0 ||
+    snakeY > DIMENSIONS.HEIGHT / 20;
+
+  const hasCollisionWithItself = snake.some(
+    ({ x, y }, idx) => idx !== 0 && snakeX === x && snakeY === y
+  );
+
+  if (isOutOfBounds || hasCollisionWithItself) {
+    console.log('game over!');
+  }
+
+  if (snakeX === food.x && snakeY === food.y) {
+    snake.push({ x: food.x, y: food.y });
+    clearFood();
+    generateFood();
+  }
+}
+
+function paintCell(x, y) {
+  canvasContext.fillStyle = 'blue';
+  canvasContext.fillRect(x * 20, y * 20, 20, 20);
+  canvasContext.strokeStyle = 'white';
+  canvasContext.strokeRect(x * 20, y * 20, 20, 20);
+}
+
+initializeGame();
